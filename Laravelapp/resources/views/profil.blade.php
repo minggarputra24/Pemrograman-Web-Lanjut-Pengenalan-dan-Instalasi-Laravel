94b85ec43ed6450238fc6d85565702c0d771b550
minggarputra24 <minggarputra24@gmail.com>
2020-04-06 09:17:15 +0700

    JobSheet9 PWL : Pengantar dan Instalasi Laravel
    JobSheet9 PWL : Pengantar dan Instalasi Laravel

---
+++ Laravelapp/resources/views/profil.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html> 
+<html> 
+    <head>            
+        <title>Profil Saya</title> 
+    </head> 
+    <body>
+        <h1>Profil Saya</h1>
+        <p>Perkenalkan Nama saya Minggar Putra Dhea Ramadhan, saya seorang Mahasiswa Jurusan Teknologi Informasi Prodi D-IV Teknik Informatika.</p> 
+    </body> 
+</html> 
+ 
